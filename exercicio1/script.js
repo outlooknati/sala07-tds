ba2319182c3d9e7e41940432d4b806c5d9c0f205
outlooknati <natisales2107@gmail.com>
2025-07-23 08:39:53 -0300

    Add files via upload

---
+++ exercicio1/script.js
@@ -0,0 +1,37 @@
+function soma(){
+    var numero1 = document.getElementById("n1").valueAsNumber
+    var numero2 = document.getElementById("n2").valueAsNumber
+    var resposta = document.getElementById("respsoma")
+    //console.log(numero1, numero2)
+    resposta.textContent = numero1 + numero2
+    //resposta.innerHTML = numero1 + numero2
+    //resposta.innerText = numero1 + numero2
+}
+
+function sub(){
+    var numero3 = parseFloat(document.getElementById("n3").value)
+    var numero4 = parseFloat(document.getElementById("n4").value)
+    var resposta = document.getElementById("respsub")
+    
+    resposta.textContent = numero3 - numero4
+}
+
+function multi(){
+    var numero5 = document.getElementById("n5").valueAsNumber
+    var numero6 = document.getElementById("n6").valueAsNumber
+    var resposta = document.getElementById("respmulti")
+    
+    resposta.textContent = numero5 * numero6
+}
+
+function div(){
+    var numero7 = document.getElementById("n7").valueAsNumber
+    var numero8 = document.getElementById("n8").valueAsNumber
+    var resposta = document.getElementById("respdiv")
+    
+    if(numero8 !== 0){
+        resposta.textContent = numero7 / numero8
+    }else{
+        resposta.textContent = "não se divide por 0"
+    }
+}
