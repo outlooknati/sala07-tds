843bff4fbdea1bad5b220afd87b73b3998dbc910
outlooknati <natisales2107@gmail.com>
2025-07-23 11:32:39 -0300

    Add files via upload

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -35,3 +35,13 @@ function div(){
         resposta.textContent = "não se divide por 0"
     }
 }
+
+function media(){
+    var n1 = document.getElementById("n1").valueAsNumber
+    var n2= document.getElementById("n2").valueAsNumber
+    var n3 = document.getElementById("n3").valueAsNumber
+    var n4 = document.getElementById("n4").valueAsNumber
+    var n5 = document.getElementById("n5").valueAsNumber
+    var resposta = document.getElementById("respmedia")
+    resposta.textContent = (n1 + n2 + n3 + n4 + n5)/5
+}
